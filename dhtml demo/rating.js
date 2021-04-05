04ac3a99d2422d99d61259672cd02c38cbf7a80d
awesomepi <33388380+awesomepi@users.noreply.github.com>
2021-04-04 22:51:52 -0400

    finished rating widget

--- dhtml demo/rating.js
+++ dhtml demo/rating.js
@@ -3,15 +3,31 @@ var emp = "emptystar.png";
 var full = "fullstar.png";
 var i;
 var j;
+var set = 0;
 
 function resetRate(){
-	for (i=1;i<=10;i++){
-		document.getElementById(i).src = emp;
+	if (set == 0){
+		for (i=1;i<=10;i++){
+			document.getElementById(i).src = emp;
+		}
+		document.getElementById("rating").innerHTML = "-";
 	}
 }
 
+function resetStar(j){
+	document.getElementById(j).src = emp;
+}
+
 function changeRate(j){
-	for (i=1;i<=j;i++){
-		document.getElementById(i).src = full;
+	if (set == 0){
+		for (i=1;i<=10;i++){
+			if (i<=j){document.getElementById(i).src = full;}
+			if (i>j){document.getElementById(i).src = emp;}
+		}
+		document.getElementById("rating").innerHTML = j;
 	}
+}
+
+function setRate(){
+	set = 1-set;
 }
